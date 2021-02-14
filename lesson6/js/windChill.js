86de785530ec5a71443d17a40d88f0384754785d
rlkilger <rlkilger@gmail.com>
2021-02-14 13:59:23 -0700

    javascript update

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,4 +1,7 @@
 let temp = parseInt(document.getElementById("temp").innerHTML);
 let windSpeed = parseInt(document.getElementById("windSpeed").innerHTML);
-let result = Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16));
+let result = "N/A";
+if ((temp < 50) && (windSpeed > 3)) {
+  result = (Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16))) + "&#176;";
+}
 document.getElementById("windChill").innerHTML = result;
