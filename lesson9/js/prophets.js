96478c511860c3f3dfd8b0c3e9967e6d6017d030
rlkilger <rlkilger@gmail.com>
2021-03-09 15:19:00 -0700

    lesson 10 and prophets update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -9,23 +9,23 @@ fetch(requestURL)
     
     const cards = document.querySelector(".cards");
     
-    prophets.forEach(prophet => {
+    for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let birthDate = document.createElement("p");
       let birthPlace = document.createElement("p");
       let image = document.createElement("img");
 
-      h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
-      birthDate.innerHTML = `Date of Birth: ${prophet.birthdate}`;
-      birthPlace.innerHTML = `Place of Birth: ${prophet.birthplace}`;
-      image.setAttribute("src", prophet.imageurl)
-      image.setAttribute("alt", `Photo of ${prophet.name} ${prophet.lastname}`)
+      h2.innerHTML = `${prophets[i].name} ${prophets[i].lastname}`;
+      birthDate.innerHTML = `Date of Birth: ${prophets[i].birthdate}`;
+      birthPlace.innerHTML = `Place of Birth: ${prophets[i].birthplace}`;
+      image.setAttribute("src", prophets[i].imageurl)
+      image.setAttribute("alt", `${prophets[i].name} ${prophets[i].lastname} - ${i + 1}`)
 
       card.append(h2);
       card.append(birthDate);
       card.append(birthPlace);
       card.append(image);
       cards.append(card);
-    });
+    };
   });
