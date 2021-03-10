aef82a72deae4fb5cd2e2feae54e3e765d98d83e
rlkilger <rlkilger@gmail.com>
2021-03-10 11:22:13 -0700

    weatherapi initial

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,18 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=070bcaa0336766a1faa1048ce4e2a875";
+
+
+
+fetch(apiURL)
+  .then((response) =>
+    response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+
+    document.getElementById("current-temp").textContent = jsObject.main.temp;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
+
