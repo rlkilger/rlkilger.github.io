38c8ee57b7e253e2550d453415df3e7d6da55b61
rlkilger <rlkilger@gmail.com>
2021-03-12 15:06:27 -0700

    fixed todaa's date display

--- lesson10/js/javascript.js
+++ lesson10/js/javascript.js
@@ -3,6 +3,17 @@ function toggleMenu() {
   document.getElementById("navigation").classList.toggle("hide");
 }
 
+//Displaay today's date
+function today() {
+  let dayOfWeek = new Date().toLocaleString('default', {weekday: 'long'});
+  let day = new Date().getDate();
+  let month = new Date().toLocaleString('default', {month: 'long'});
+  let year = new Date().getFullYear();
+  document.getElementById("currentDate").innerHTML = `${dayOfWeek}, ${day} ${month} ${year}`;
+}
+
+today();
+
 //Storm severity rating
 function adjustRating(severity) {
   document.getElementById("rating").innerHTML = severity;
