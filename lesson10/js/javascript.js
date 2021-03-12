ce734546df6b625371f54ad422af8ab8e57bd8ae
rlkilger <rlkilger@gmail.com>
2021-03-12 12:42:11 -0700

    menu fix

--- lesson10/js/javascript.js
+++ lesson10/js/javascript.js
@@ -3,17 +3,6 @@ function toggleMenu() {
   document.getElementById("navigation").classList.toggle("hide");
 }
 
-//Date last modified
-function lastModified() {
-  let dayOfWeek = new Date().toLocaleString('default', {weekday: 'long'});
-  let day = new Date().getDate();
-  let month = new Date().toLocaleString('default', {month: 'long'});
-  let year = new Date().getFullYear();
-  document.getElementById("currentDate").innerHTML = `${dayOfWeek}, ${day} ${month} ${year}`;
-}
-
-lastModified();
-
 //Storm severity rating
 function adjustRating(severity) {
   document.getElementById("rating").innerHTML = severity;
