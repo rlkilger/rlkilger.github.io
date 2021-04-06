a4333e9ef8d2497ee7cdb28a67f24e3ab7bea077
rlkilger <rlkilger@gmail.com>
2021-04-05 19:31:50 -0600

    directory grid/list

--- cedarhillschamber/js/businesses.js
+++ cedarhillschamber/js/businesses.js
@@ -4,59 +4,55 @@ fetch("json/businesses.json")
   })
   .then(function (jsonObject) {
     const businesses = jsonObject["businesses"];
-    console.log(businesses);
     
-    const cards = document.querySelector(".cards");
+    const cards = document.querySelector("#cards");
     
     for (let i = 0; i < businesses.length; i++ ) {
       let div = document.createElement("div");
-      let logo = document.createElement("a");
-      let spanDiv = document.createElement("div");
       let image = document.createElement("img");
+      let logoAnchor = document.createElement("a");
+      let imgDiv = document.createElement("div");
+      let spanDiv = document.createElement("div");
       let address = document.createElement("span");
       let phone = document.createElement("span");
-      let anchor = document.createElement("a");
       let hours = document.createElement("span");
       let email = document.createElement("span");
-      let webAnchor = document.createElement("a");
       let website = document.createElement("span");
+      let webAnchor = document.createElement("a");
 
-      div.setAttribute("class", `card`);
+      div.setAttribute("class", `card grid-card`);
+      imgDiv.setAttribute("class", `img-div`);
 
       if (businesses[i].imageurl != undefined) {
-        logo.setAttribute("class", `img-div`);
-        logo.setAttribute("href", `${businesses[i].website}`)
-        logo.setAttribute("target", `_blank`);
+        logoAnchor.setAttribute("class", `img-div`);
+        logoAnchor.setAttribute("href", `${businesses[i].web}`)
+        logoAnchor.setAttribute("target", `_blank`);
         image.setAttribute("src", businesses[i].imageurl);
         image.setAttribute("alt", `${businesses[i].name}` + " logo");
+        logoAnchor.append(image);
       } else {
-        logo.innerHTML = "<h4>" + `${businesses[i].name}` + "</h4>";
+        logoAnchor.innerHTML = "<h4>" + `${businesses[i].name}` + "</h4>";
       }
 
       spanDiv.setAttribute("class", `span-div`);
       address.innerHTML = `${businesses[i].address}`;
-      
-      anchor.setAttribute("href", `tel:${businesses[i].href}`)
-      anchor.innerHTML = `${businesses[i].phone}`;
+      phone.innerHTML = `${businesses[i].phone}`;
 
       if (businesses[i].email != undefined) {
-        webAnchor.setAttribute("href", `mailto:${businesses[i].email}`)
-        webAnchor.innerHTML = `${businesses[i].email}`;
+        email.innerHTML = `${businesses[i].email}`;
       }
 
+      webAnchor.setAttribute("href", `mailto:${businesses[i].web}`)
       website.innerHTML = `${businesses[i].website}`;
       
 
-
-      logo.append(image);
-      div.append(logo);
+      div.append(imgDiv);
+      imgDiv.append(logoAnchor);
       spanDiv.append(address);
-      phone.append(anchor)
       spanDiv.append(phone);
-      spanDiv.append(hours);
-      email.append(webAnchor);
       spanDiv.append(email);
-      spanDiv.append(website);
+      webAnchor.append(website);
+      spanDiv.append(webAnchor);
       div.append(spanDiv);
       cards.append(div);
     };
--- cedarhillschamber/js/javascript.js
+++ cedarhillschamber/js/javascript.js
@@ -9,6 +9,17 @@ function lastModified() {
   document.getElementById("lastMod").innerHTML = document.lastModified;
   document.getElementById("year").innerHTML = new Date().getFullYear();
 }
-
 lastModified();
 
+
+//Display grid
+function displayGrid() {
+  document.getElementById("cards").classList.add("grid");
+  document.getElementById("cards").classList.remove("list");
+}
+
+//Display list
+function displayList() {
+  document.getElementById("cards").classList.add("list");
+  document.getElementById("cards").classList.remove("grid");
+}
